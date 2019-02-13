8b711f58c513980c9c7dd249422d1b2920a3687e
Leonardo Ochoa <leo8a11@hotmail.com>
2019-02-12 19:04:23 -0600

    Creacion Metodo POST

--- EX10801199720404/routes/api/things.js
+++ EX10801199720404/routes/api/things.js
@@ -4,6 +4,15 @@ var router = express.Router();
 var fileModel = require('./examenmodel');
 var data = null;
 
+var datosEmpresa = {
+    'RTN':'',
+    'Empresa':'',
+    'Correo':'',
+    'Rubro':'',
+    'Direccion':'',
+    'Telefono':''
+}
+
 
 router.get('/', function(req, res, next){
     if(!data){
@@ -21,6 +30,23 @@ router.get('/', function(req, res, next){
 
     }    
 }); // Metodo GET
+
+router.post('/new', function(req, res, next){
+    
+    var _thingsData = Object.assign({}, datosEmpresa, req.body);
+    _thingsData._id = uuidv4();
+    if(!data){
+        data = [];
+    }
+    data.push(_thingsData);
+    fileModel.write(data, function(err){
+        if(err){ 
+            console.log(err);
+            return res.status(500),json({'error': 'Error al obtener data'});
+        }
+        return res.status(200).json(_thingsData);
+    });   
+}); // Metodo POST
 
 
 fileModel.read(function(err, filedata){
